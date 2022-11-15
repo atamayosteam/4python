7b3ff4b1a55909221f4d0b8496cb7404e9631d43
atamayosteam <113442415+atamayosteam@users.noreply.github.com>
2022-11-15 10:34:26 -0500

    s

---
+++ loops.py
@@ -0,0 +1,20 @@
+# x = ''
+
+# while x != 'stop':
+#     print('hello world')
+#    x = input('type stop to exit')
+
+ 
+ 
+#     y = 1
+#     while y <= 10:
+#     y  = y + 1
+#     print(y)
+
+# groceries = ['apple','pera','grape','cucumber']
+
+# for z in groceries:
+#     print(z)
+
+for i in range(1,11):
+    print(i)
