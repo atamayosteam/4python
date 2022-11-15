9af4367ce311f4e41dcf900007f74dd9fe4650a8
atamayosteam <113442415+atamayosteam@users.noreply.github.com>
2022-11-15 09:32:11 -0500

    h

---
+++ lists.py
@@ -0,0 +1,17 @@
+x = ['apple','orange','pear','grape','watermelon']
+
+print(x)
+
+x.append('strawberry')
+print(x[2])
+
+x[0] = 'fruit'
+print(x)
+
+del x[2]
+print(x)
+
+my_tuple = ('blue','red','green','yellow')
+my_tuple[2] = 'navy'
+print(my_tuple)
+#
