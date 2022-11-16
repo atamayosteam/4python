8dd26e984b137d25aa284e3b43f2313facbf93e4
atamayosteam <113442415+atamayosteam@users.noreply.github.com>
2022-11-16 09:28:57 -0500

    f

--- lists.py
+++ lists.py
@@ -11,7 +11,31 @@ print(x)
 del x[2]
 print(x)
 
-my_tuple = ('blue','red','green','yellow')
-my_tuple[2] = 'navy'
-print(my_tuple)
-#
+# my_tuple = ('blue','red','green','yellow')
+# my_tuple[2] = 'navy'
+# print(my_tuple)
+
+print(len(x))
+
+x.sort()
+
+print(x)
+
+x.reverse()
+
+print(x)
+
+y = ['brown','red','blue','pink','orange']
+
+
+print( x + y)
+
+#combined two lists into one lists 
+
+print('  '.join(x + y)) 
+
+# removed the commas and quotes by using the .join function
+
+names = ("john,sam,bob".split(","))
+
+print(names)
