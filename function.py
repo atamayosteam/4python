59f8070b6058a9d3ab0021c7dfb8d4dbd7ff4fb8
atamayosteam <113442415+atamayosteam@users.noreply.github.com>
2022-11-16 10:17:12 -0500

    function

---
+++ function.py
@@ -0,0 +1,42 @@
+def hello_world():
+    print('hello world')
+
+hello_world()
+
+def add(a, b):
+    print(a + b)
+    
+    return a + b
+
+x = add(9, 10)
+
+print(x)
+
+def divide(a, b):
+    print(a /b)
+
+divide(12, 4)
+
+def multiply(a, b):
+    print(a * b)
+
+multiply(12, 4)
+
+def remainder(a, b):
+    print(a // b)
+
+remainder(14, 6)
+
+def exponent(a, b):
+    print(a ** b)
+
+exponent(2, 4)
+
+# add, multiply, remainer, exponent
+
+def slope(m , x, b):
+    return(m * x + b)
+
+y = slope(5,8, 7)
+
+print(y)
