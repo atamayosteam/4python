8a79b8557f981abe8ba31b87d9573f31e09f6509
atamayosteam <113442415+atamayosteam@users.noreply.github.com>
2022-11-21 09:25:44 -0500

    xx

--- average.py
+++ average.py
@@ -1,9 +1,31 @@
-x = [99, 99, 80]
+
+
+grades = []
+
+x = input('Add numbers: ')
+grades.append(int(x))
+
+x = input('Add numbers: ')
+grades.append(int(x))
+
+x = input('Add numbers: ')
+grades.append(int(x))
+
+x = input('Add numbers: ')
+grades.append(int(x))
+
+x = input('Add numbers: ')
+grades.append(int(x))
+
+x = input('Add numbers: ')
+grades.append(int(x))
 
 
 sum = 0
 
-for grade in x:
+for grade in grades:
     sum = sum + grade
 
-print(sum / len(x))
+print(sum / len(grades))
+
+print(grades)
