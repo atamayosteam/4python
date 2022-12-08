3a2e98b7372a58be67781092049586276b318391
atamayosteam <113442415+atamayosteam@users.noreply.github.com>
2022-12-08 10:55:10 -0500

    b

--- classes.py
+++ classes.py
@@ -9,6 +9,10 @@ class person:
     def hi(self):
       print("Hi my name is " + self.name)
 
+class student(person):
+    role = 'student'
+    
+
 Anthony = person("AnthonyT", 17)
 print(Anthony.species)
 print(Anthony.name)
@@ -20,3 +24,17 @@ print(Monuiel.age)
 # def hi(self):
 #     print("Hi my name is" + self.name)
 
+class Teacher(person):
+    role = 'Teacher'
+
+    def hi(self):
+      print("Hi my name is Mr. " + self.name)
+
+
+Forlenza = Teacher("Forlenza", 184)
+print(Forlenza.role)
+
+Forlenza.hi()
+
+# class student(person):
+#     role = 'student'
